4abdfc6446fab83e3563d07660d71b13fb9e5b81
sekuchu <brankosekulovic17@gmail.com>
2025-08-07 18:13:57 +0200

    finished player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
@@ -8,12 +9,36 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    # Create groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Assign groups to Player class
+    Player.containers = (updatable, drawable)
+
+    # Create player (added automatically to groups)
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(color="black")
+        
+
+        # Update all updatable sprites
+        updatable.update(dt)
+
+        # Draw all drawable sprites
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         
         Clock().tick(60)
